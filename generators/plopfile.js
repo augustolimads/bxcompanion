a109f338da9215218bc9f31f03b11ade699f91ea
Augusto Lima <augustolimads@gmail.com>
2022-10-23 16:12:02 -0300

    chore: remove minimal plop config

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,36 +1,4 @@
 module.exports = (plop) => {
-  plop.setGenerator("component", {
-    description: "Create a component",
-    prompts: [
-      {
-        type: "input",
-        name: "name",
-        message: "What is your component name?",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "../src/components/{{pascalCase name}}/index.ts",
-        templateFile: "templates/Index.ts.hbs",
-      },
-      {
-        type: "add",
-        path: "../src/components/{{pascalCase name}}/index.tsx",
-        templateFile: "templates/Component.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.types.ts",
-        templateFile: "templates/types.ts.hbs",
-      },
-      {
-        type: "append",
-        path: "../src/components/index.ts",
-        template: "export { {{pascalCase name}} } from './{{pascalCase name}}';",
-      }
-    ],
-  }),
   plop.setGenerator("full component", {
     description: "Create a full component",
     prompts: [

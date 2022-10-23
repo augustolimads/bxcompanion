899e474c5ecb7ae9048c7fb42d5ae1ac1a98c5db
Augusto Lima <augustolimads@gmail.com>
2022-10-22 21:12:39 -0300

    chore: first commit

---
+++ generators/plopfile.js
@@ -0,0 +1,61 @@
+module.exports = (plop) => {
+    plop.setGenerator("component", {
+      description: "Create a component",
+      prompts: [
+        {
+          type: "input",
+          name: "name",
+          message: "What is your component name?",
+        },
+      ],
+      actions: [
+        {
+          type: "add",
+          path: "../src/components/{{pascalCase name}}/index.tsx",
+          templateFile: "templates/Component.tsx.hbs",
+        }
+      ],
+    }),
+    plop.setGenerator("full component", {
+      description: "Create a full component",
+      prompts: [
+        {
+          type: "input",
+          name: "name",
+          message: "What is your component name?",
+        },
+      ],
+      actions: [
+        {
+          type: "add",
+          path: "../src/components/{{pascalCase name}}/index.ts",
+          templateFile: "templates/Index.ts.hbs",
+        },
+        {
+          type: "add",
+          path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+          templateFile: "templates/Component.tsx.hbs",
+        },
+        {
+          type: "add",
+          path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.types.ts",
+          templateFile: "templates/types.ts.hbs",
+        },
+        {
+          type: "add",
+          path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.viewmodel.ts",
+          templateFile: "templates/viewmodel.ts.hbs",
+        },
+        {
+          type: "add",
+          path: "../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.ts",
+          templateFile: "templates/Test.spec.ts.hbs",
+        },
+        {
+          type: "append",
+          path: "../src/components/index.ts",
+          template: "export { {{pascalCase name}} } from './{{pascalCase name}}';",
+        }
+      ],
+    })
+  };
---
+++ jest.config.js
@@ -0,0 +1,23 @@
+// jest.config.js
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+})
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  coverageThreshold: {
+    global: {
+      lines: 80
+    }
+  }
+}

816fdb1fbed4ba9170e4c20ff1edf443e8772031
Augusto Lima <augustolimads@gmail.com>
2022-11-15 00:04:17 -0300

    fix: fix pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,9 +6,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content={theme.colors.primary[500]} />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="description of your project" />
           <link rel="manifest" href="pwa/manifest.json" />
           <link rel="apple-touch-icon" href="pwa/icon-192x192.png" />
-          <meta name="theme-color" content={theme.colors.primary[500]} />
+          <link rel="shortcut icon" href="/favicon-16x16.ico" />
+          <title>BX Companion</title>
         </Head>
         <body>
           <Main />

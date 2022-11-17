17d5c82cdccae229ce135a82302c0c9d769d200f
Augusto Lima <augustolimads@gmail.com>
2022-11-16 22:57:53 -0300

    fix(attrEditor): add missed type

--- src/components/AttributeListEditor/AttributeListEditor.types.ts
+++ src/components/AttributeListEditor/AttributeListEditor.types.ts
@@ -3,3 +3,13 @@ export type AttributeListEditorProps = {
     attrRef: "str" | "dex" | "con" | "int" | "wis" | "cha"
     label: string
 }
+
+
+export type EditAttributeProps = {
+    str: number
+    dex: number
+    con: number
+    int: number
+    wis: number
+    cha: number
+}

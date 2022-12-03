d4ad280adccb35d557b9093b2374d38917724f96
Augusto Lima <augustolimads@gmail.com>
2022-12-03 17:49:21 -0300

    feat(icons): add hand icon

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -3,7 +3,7 @@ export {
     BsFillGearFill as ConfigIcon,
     BsThreeDotsVertical as ThreeDotsIcon
 } from 'react-icons/bs'
-export { FaStar as StarIcon } from 'react-icons/fa'
+export { FaRegHandRock as HandIcon, FaStar as StarIcon } from 'react-icons/fa'
 export { GiDiceTwentyFacesTwenty as DiceIcon } from 'react-icons/gi'
 export { MdOutlineMenu as MenuIcon } from 'react-icons/md'
 export { RiPencilFill as PencilIcon } from 'react-icons/ri'

41e3a9c1f5c013bcf95226b4726aa3353890e012
Augusto Lima <augustolimads@gmail.com>
2022-12-03 17:46:27 -0300

    refactor(theme): add global style

---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    neutral: {
+        900: "#2C3038",
+        700: "#3F444F",
+        500: "#535A67",
+        300: "#747D8E",
+        100: "#CACACA",
+    },
+    primary: {
+        900: "#4B3C4C",
+        500: "#692D3D"
+    },
+    secondary: {
+        500: "#0AEFC6",
+        100: "#DDF5ED"
+    },
+    yellow: "#FFCD1E",
+    green: "#74A634",
+    blue: "#3294CB"
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,24 +1,13 @@
 import { extendTheme } from "@chakra-ui/react"
+import { colors } from './colors'
 
 export const theme = extendTheme({
-  colors: {
-    neutral: {
-        900: "#2C3038",
-        700: "#3F444F",
-        500: "#535A67",
-        300: "#747D8E",
-        100: "#CACACA",
-    },
-    primary: {
-        900: "#4B3C4C",
-        500: "#692D3D"
-    },
-    secondary: {
-        500: "#0AEFC6",
-        100: "#DDF5ED"
-    },
-    yellow: "#FFCD1E",
-    green: "#74A634",
-    blue: "#3294CB"
+  colors,
+  styles:{
+    global:{
+      option: {
+          background: colors.neutral[900]
+      }
+    }
   },
 })
